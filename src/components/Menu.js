c460e0dc3773b6f12d199bca6ff543fb34c7b382
Dmitry Diakov <d.diakov@list.ru>
2021-11-30 23:12:08 +0500

    initial commit

---
+++ src/components/Menu.js
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom';
+
+export default function Menu() {
+    return (
+      <nav className="menu">
+        <NavLink exact
+          to="/"
+          className="menu__item"
+          activeClassName="menu__item menu__item-active"
+        >
+          Главная
+        </NavLink>
+        <NavLink
+          to="/drift"
+          className="menu__item"
+          activeClassName="menu__item menu__item-active"
+        >
+          Дрифт-такси
+        </NavLink>
+        <NavLink
+          to="/timeattack"
+          className="menu__item"
+          activeClassName="menu__item menu__item-active"
+        >
+          Time Attack
+        </NavLink>
+        <NavLink
+          to="/forza"
+          className="menu__item"
+          activeClassName="menu__item menu__item-active"
+        >
+          Forza Karting
+        </NavLink>
+      </nav>
+    );
+}
